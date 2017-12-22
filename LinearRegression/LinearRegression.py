d467acf2a72418f23c1da428bc2d2a2d079e9b28
siddharth kathuria <kathuria.siddharth@yahoo.in>
2017-12-23 02:32:41 +0530

    linear regression

---
+++ LinearRegression/LinearRegression.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 23 02:01:25 2017
+
+@author: sidkathuria14
+"""
+
+import numpy as np
+import pandas as pd
+import sys
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Salary_Data.csv')
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,1].values
+
+from sklearn.cross_validation import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,train_size = 0.2,random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regresso = LinearRegression()
+regresso.fit(x_train,y_train)
+
+y_pred = regresso.predict(x_test)
+
+plt.scatter(x_train,y_train,color = 'red')
+plt.plot(x_test,regresso.predict(x_test),color = 'blue')
+plt.title('Salary vs. Experience')
+plt.xlabel('Experience')
+plt.ylabel('Salary')
+plt.show()
