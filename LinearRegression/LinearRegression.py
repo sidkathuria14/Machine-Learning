f5a257e4033ecac708b966ca3b56dbdf72d28d95
siddharth kathuria <kathuria.siddharth@yahoo.in>
2017-12-25 00:19:33 +0530

    minor fault i linear regression

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -16,7 +16,7 @@ x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,1].values
 
 from sklearn.cross_validation import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x,y,train_size = 0.2,random_state = 0)
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2,random_state = 0)
 
 from sklearn.linear_model import LinearRegression
 regresso = LinearRegression()
