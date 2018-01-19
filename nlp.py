7c42321a73383526c56633735121939460529dfd
siddharth kathuria <kathuria.siddharth@yahoo.in>
2018-01-20 00:31:50 +0530

    nlp added

---
+++ nlp.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 23:41:39 2018
+
+@author: sidkathuria14
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('/home/sidkathuria14/Desktop/desktop/ML Code/Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+x = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:,1].values
+from sklearn.cross_validation import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,train_size = 0.8,random_state = 42)
+from sklearn.naive_bayes import GaussianNB
+y_pred = classifier.predict(x_test)
+classifier = GaussianNB()
+classifier.fit(x_train,y_train)
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
